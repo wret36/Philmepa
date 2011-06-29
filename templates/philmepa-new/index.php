0f8a90cc86cbf24fde4c61ea0e91eb0fb6faeae7
Christian <cris@98Labs-Inc-001.(none)>
2011-06-29 22:58:58 +0800

    add new template for the new design

---
+++ templates/philmepa-new/index.php
@@ -0,0 +1,122 @@
+<?php $templates = $this->baseurl . "/templates/philmepa-new"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>TemplateWorld.com Template - Industrial</title>
+<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+ <div id="logo_banner">
+   <div class="menu_area">
+	 <jdoc:include type="modules" name="primary-menu" />
+   </div>
+  </div>
+  <div id="calendar">
+  <div class="calendar_area">
+    <div class="date">
+      <div align="center">
+        <p class="date_head">16</p>
+        <p>        january</p>
+      </div>
+    </div>
+    <div class="date_text">
+      <p class="date_head1">Industrial Seminar</p>
+      <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain...</p>
+      <p><a href="#" class="datelink">+ more</a></p>
+    </div>
+  </div>
+  <div class="calendar_area">
+    <div class="date">
+      <div align="center">
+        <p class="date_head">28</p>
+        <p> january</p>
+      </div>
+    </div>
+    <div class="date_text">
+      <p class="date_head1">Deprovatix Inauguration</p>
+      <p>To take a trivial example, which of us ever undertakes laborious physical exercise which toil procure him great...</p>
+      <p><a href="#" class="datelink">+ more</a></p>
+    </div>
+  </div>
+  <div class="calendar_area">
+    <div class="date">
+      <div align="center">
+        <p class="date_head">07</p>
+        <p> february</p>
+      </div>
+    </div>
+    <div class="date_text">
+      <p class="date_head1">Optimal Delivery</p>
+      <p>On the other hand, we denounce with righteous indignation and dislike men who are so...</p>
+      <p><a href="#" class="datelink">+ more</a></p>
+    </div>
+  </div>
+  </div>
+  <div id="banner">
+    <div class="banner_textarea"> 
+      <p class="banner_head">Libero Tempor Soluta Nobis</p>
+      <p class="banner_head1">Itaque earum rerum hic tenetur a sapiente</p>
+      <p></p>
+      <p>When our power of choice is untrammelled and when nothing prevents our being able to do what we like best. But in certain circumstances and owing to the claims <br />
+      of duty or the obligations of business.</p>
+      <p></p>
+      <div class="search_area">
+        <div class="searchname">Industry Search</div>
+        <div class="searchbox">
+          <label>
+          <input name="textfield" type="text" class="search_textbox" />
+          </label>
+        </div>
+        <div class="searchgo">
+          <div align="center"><a href="#" class="go">GO</a></div>
+        </div>
+      </div>
+      <br/>
+    </div>
+  </div>
+  <div id="body_area">
+    <div class="industrialist">
+      <div class="head1">Industrialist of this Month <span class="head2">Repudiandae sint et molestiae non recusandae</span></div>
+      <div class="industrialist_pic">
+        <div align="justify">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animist.</div>
+      </div>
+      <div class="industrialist_more">Reiciendis voluptatibus maiores alias consequatur <a href="#" class="more_link">+more</a> </div>
+    </div>
+    <div class="industry1">
+      <div class="head3">
+        <div align="center">molestias industry</div>
+      </div>
+      <div class="industry_text">
+        <p align="justify"><span class="head4">In a free hourwhen our power of choice</span> is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. </p>
+        <br/>
+        <p><a href="#" class="more_link1">+more</a></p>
+      </div>
+    </div>
+    <div class="industry1">
+      <div class="head3">
+        <div align="center">repudiane industry</div>
+      </div>
+      <div class="industry_text">
+        <p align="justify"><span class="head4">The wise man therefore always holds</span> On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will end.. </p>
+        <br/>
+        <p><a href="#" class="more_link1">+more</a></p>
+      </div>
+    </div>
+  </div>
+  <div id="fotter">
+    <div class="fotter_links">
+      <div align="center"><a href="#" class="fotterlink">home</a>  :  <a href="#" class="fotterlink">history</a>  :  <a href="#" class="fotterlink">services</a>  :  <a href="#" class="fotterlink">news</a>  :  <a href="#" class="fotterlink">sitemap</a>  :  <a href="#" class="fotterlink">contact</a></div>
+    </div>
+    <div class="fotter_copyrights">
+      <div align="center">&copy; Copyright Information Goes Here. All Rights Reserved.</div>
+    </div>
+    <div class="fotter_validation">
+      <div align="center"><a href="http://validator.w3.org/check?uri=referer" target="_blank" class="xhtml">XHTML</a> <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank" class="css">CSS</a><br />
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
