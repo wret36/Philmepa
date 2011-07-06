b22a76699641bfa62abf98fa693cdb5d8f038f55
Christian <cris@98Labs-Inc-001.(none)>
2011-07-06 12:23:15 +0800

    change design of the inner content

--- templates/philmepa-new/index.php
+++ templates/philmepa-new/index.php
@@ -1,10 +1,17 @@
 <?php $templates = $this->baseurl . "/templates/philmepa-new"; ?>
+<?php 
+	$pageoption = JRequest::getVar('option', '');
+	$pageview = JRequest::getVar('view', '');
+	$pageitemid = JRequest::getVar('Itemid', '');
+	$pageid = JRequest::getVar('id', '');
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>TemplateWorld.com Template - Industrial</title>
 <link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/style.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/innerstyle.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -13,7 +20,10 @@
 	 <jdoc:include type="modules" name="primary-menu" />
    </div>
   </div>
-	<jdoc:include type="modules" name="latest-news" />
+	<div id="calendar">
+		<jdoc:include type="modules" name="latest-news" />
+	</div>
+	<?php if($pageview == "frontpage") :?>
   <div id="banner">
     <div class="banner_textarea"> 
       <p class="banner_head">Libero Tempor Soluta Nobis</p>
@@ -40,27 +50,20 @@
     <div class="home-content">
     	<jdoc:include type="component" />
     </div>
-    <div class="industry1">
-      <div class="head3">
-        <div align="center">molestias industry</div>
-      </div>
-      <div class="industry_text">
-        <p align="justify"><span class="head4">In a free hourwhen our power of choice</span> is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. </p>
-        <br/>
-        <p><a href="#" class="more_link1">+more</a></p>
-      </div>
-    </div>
-    <div class="industry1">
-      <div class="head3">
-        <div align="center">repudiane industry</div>
-      </div>
-      <div class="industry_text">
-        <p align="justify"><span class="head4">The wise man therefore always holds</span> On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will end.. </p>
-        <br/>
-        <p><a href="#" class="more_link1">+more</a></p>
-      </div>
-    </div>
+    <jdoc:include type="modules" name="take-action-and-sponsors" style="takeActonAndSponsors" headerLevel="5"  />
   </div>
+  <?php else :?>
+	<div id="main-content">
+		<div id="innerbody_area">
+			<div class="industries_area">
+				<jdoc:include type="modules" name="take-action-and-sponsors" style="takeActonAndSponsors" headerLevel="5"  />  	
+			</div>
+			<div class="right_textarea">
+				<jdoc:include type="component" /> 	
+			</div>
+		</div>
+	</div>
+  <?php endif;?>
   <div id="fotter">
     <div class="fotter_links">
       <div align="center"><a href="#" class="fotterlink">home</a>  :  <a href="#" class="fotterlink">history</a>  :  <a href="#" class="fotterlink">services</a>  :  <a href="#" class="fotterlink">news</a>  :  <a href="#" class="fotterlink">sitemap</a>  :  <a href="#" class="fotterlink">contact</a></div>
